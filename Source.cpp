4f11aa54e6598da8eac517d007ea691009bc288c
mmanthei <micahthemanthei@gmail.com>
2016-03-28 06:55:34 -0400

    Functions to Create
    The functions to create for edX CS101 Graded Programming Assignment 2.

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+/* Question          : count number of students scoring X marks in GQ and store it in 'sum_gq'
+int numOfStudents : Integer number containing the total number of students
+int gq[]          : Integer array containing the GQ marks of all students
+int sum_gq[]      : Integer array to be computed  */
+void getTotalGQ(int numOfStudents, int gq[], int sum_gq[]) {
+	// Write your solution code below this line
+
+
+}
+
+
+
+/* Question          : Count number of students scoring Y marks in GPA and store it in 'sum_gpa'
+int numOfStudents : Integer number containing the total number of students
+float gpa[]       : Array of type float containing the GPA marks of all students
+int sum_gpa[]     : Integer array to be computed */
+void getTotalGPA(int numOfStudents, float gpa[], int sum_gpa[]) {
+	// Write your solution code below this line
+
+
+}
+
+
+
+/* Question          : Count number of students scoring X marks in GQ, Y marks in GPA, total, and result, in 'count[][]'
+int numOfStudents : Integer number containing the total number of students
+int gq[]          : Integer array containing the GQ marks of all students
+float gpa[]       : Array of type float, containing the GPA marks of all students
+int count[][]     : Integer array to be computed */
+void getTotalCount(int numOfStudents, int gq[], float gpa[], int count[][5]) {
+	// Write your solution code below this line
+
+
+}
